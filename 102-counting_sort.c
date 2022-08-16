f91c3b382d49bd085967eb3357d22245238cba61
MaedotMellion <101402800+MaedotMellion@users.noreply.github.com>
2022-08-15 17:20:35 -0700

    Update 102-counting_sort.c

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,47 +1,44 @@
 #include "sort.h"
 
 /**
- * counting_sort - Sorts an array of integers in ascending
- * order using the Counting sort algorithm
- *
- * @array: The array to be sorted.
- * @size: The size of the array.
- *
+ * counting_sort - sorts an array with the Counting sort algorithm
+ * @array: array to sort
+ * @size: size of the array
  */
-
 void counting_sort(int *array, size_t size)
 {
-	size_t i = 1;
-	int max, j;
-	int *counter, *temp;
+	int *count_arr, *out_arr, max, num, j, l;
+	size_t i, k, m, n;
 
 	if (size < 2)
 		return;
 
 	max = array[0];
-
-	for (; i < size; i++)
-	{
+	for (i = 1; i < size; i++)
 		if (array[i] > max)
 			max = array[i];
+
+	count_arr = malloc(sizeof(size_t) * (max + 1));
+	out_arr = malloc(sizeof(int) * size);
+
+	for (j = 0; j <= max; j++)
+		count_arr[j] = 0;
+	for (k = 0; k < size; k++)
+	{
+		num = array[k];
+		count_arr[num] += 1;
 	}
-	counter = malloc(sizeof(int) * max + 1);
-	temp = malloc(sizeof(int) * size);
+	for (l = 1; l <= max; l++)
+		count_arr[l] += count_arr[l - 1];
+	print_array(count_arr, max + 1);
+	for (m = 0; m < size; m++)
+	{
+		out_arr[count_arr[array[m]] - 1] = array[m];
+		count_arr[array[m]]--;
+	}
+	for (n = 0; n < size; n++)
+		array[n] = out_arr[n];
 
-	for (i = 0; i < size; i++)
-		counter[array[i]] += 1;
-
-	for (j = 1; j <= max; j++)
-		counter[j] = counter[j] + counter[j - 1];
-
-	print_array(counter, max + 1);
-
-	for (i = 0; i < size; i++)
-		temp[counter[array[i] - 1]] = array[i];
-
-	for (i = 0; i < size; i++)
-		array[i] = temp[i];
-
-	free(temp);
-	free(counter);
+	free(count_arr);
+	free(out_arr);
 }
